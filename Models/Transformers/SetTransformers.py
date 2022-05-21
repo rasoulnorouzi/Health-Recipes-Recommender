5cf8e2f4f21b71a828197d179ce10ff9333d7fff
Rasoul Norouzi <46104045+rasoulnorouzi@users.noreply.github.com>
2022-05-22 01:46:15 +0430

    Update SetTransformers.py
    Set-Transformer is done!

--- Models/Transformers/SetTransformers.py
+++ Models/Transformers/SetTransformers.py
@@ -6,13 +6,36 @@ from TransformerBlocks import *
 
 
 class SetTransformer(nn.Module):
-    def __init__(self, n_layers, d_model, heads, d_ff, dropout):
+    def __init__(self, d_model, n_heads, n_seeds, induced_dim, d_output):
         super(SetTransformer, self).__init__()
-        pass
+        
+        '''
+        Arguments:
+            d_model: the dimension of Embedding Layer
+            n_heads: the number of heads
+            n_seeds: the number of seed vectors
+            induced_dim: the dimension of induced vector
+            d_output: the dimension of output
+        inputs:
+            X: the Embedding tensor of shape [batch_size, seq_len_X, d_model]
+        returns:
+            a float tensor of shape [batch_size, n_seeds, d_output]
 
-    def forward(self, src, trg, src_mask, trg_mask):
-        pass
+        '''
 
-
+        self.enc = nn.Sequential(
+            ISAB(d_model, n_heads, induced_dim),
+            ISAB(d_model, n_heads, induced_dim),
+        )
+        self.dec = nn.Sequential(
+            PMA(d_model, n_heads, n_seeds),
+            SAB(d_model, n_heads),
+            SAB(d_model, n_heads),
+            nn.Linear(d_model, d_output)
+            )
+    def forward(self, X):
+        X = self.enc(X)
+        X = self.dec(X)
+        return X
 
 
